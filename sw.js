b02793598ad9ba1c0fb7b5011da4062ec5c66d75
alejaa-sketch <aleja.aguila1209@gmail.com>
2026-05-03 19:24:29 -0400

    Update service worker cache version and URLs

--- sw.js
+++ sw.js
@@ -1,7 +1,14 @@
-const CACHE_NAME = 'ric-suite-v1';
+const CACHE_NAME = 'ric-suite-v2';
+const urlsToCache = [
+  './index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js',
+  'https://fonts.googleapis.com/css2?family=Share+Tech+Mono&family=Rajdhani:wght@400;600;700&family=Exo+2:wght@300;400;600&display=swap'
+];
+
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(['./index.html'])));
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
+
 self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
