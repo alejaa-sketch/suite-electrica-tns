7d78d4b2a67de9a80b6eda48d266ba6613f2cf79
alejaa-sketch <aleja.aguila1209@gmail.com>
2026-05-04 00:30:38 -0400

    Update cache name from v7 to v8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ric-suite-v7';
+const CACHE_NAME = 'ric-suite-v8';
 const urlsToCache = [
   './index.html',
   'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js',
