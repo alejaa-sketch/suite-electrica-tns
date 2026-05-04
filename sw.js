e243901b00835d893c814e568c2bc5d784407cfa
alejaa-sketch <aleja.aguila1209@gmail.com>
2026-05-03 20:21:27 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ric-suite-v3';
+const CACHE_NAME = 'ric-suite-v4';
 const urlsToCache = [
   './index.html',
   'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js',
